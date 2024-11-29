34412f1c4fa0d055696eb9e2cb0b955f9e766340
Mikiyas Birhanu <mkbirhanu@gmail.com>
2024-11-30 00:58:38 +0300

    nextjs inital setup

--- src/pages/page2.tsx
+++ src/pages/page2.tsx
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import localFont from "next/font/local";
-import Link from "next/link";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -13,32 +12,26 @@ const geistMono = localFont({
   weight: "100 900",
 });
 
-export default function Home() {
+export default function PageTwo() {
   return (
     <div
       className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
     >
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-      <h1 className="text-2xl">Nextjs 14</h1>
        
+       <h1>Nextjs 14</h1>
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
             Get started by editing{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/pages/index.tsx
+              src/pages/page2.tsx
             </code>
             .
           </li>
-          <li>Save and see your changes instantly.</li>
+          
         </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <Link href={'page2'}
-          >
-          Goto Page two
-          </Link>
-       
-        </div>
+      
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
