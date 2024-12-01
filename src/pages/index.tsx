18f8ada15b057da55948ae0451e3d1bcf6bcb4ae
Mikiyas Birhanu <mkbirhanu@gmail.com>
2024-12-02 01:00:06 +0300

    Demo build

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,12 @@ import Image from "next/image";
 import localFont from "next/font/local";
 import Link from "next/link";
 
+import { check } from "@tauri-apps/plugin-updater";
+import { relaunch } from "@tauri-apps/plugin-process";
+import { useEffect, useState } from "react";
+import MainWindowLoading from "@/components/layouts/MainWindowLoading";
+
+
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -14,6 +20,76 @@ const geistMono = localFont({
 });
 
 export default function Home() {
+  const [updateMessage, setUpdateMessage] = useState<string>(
+    "checking For Update"
+  );
+  const [isUpdating, setIsUpdating] = useState<boolean>(true);
+  const [isUpdated, setIsUpdated] = useState<boolean>(false);
+  const [updateProgress, setUpdateProgress] = useState<number>(0);
+
+  useEffect(() => {
+    const checkUpdate = async () => {
+      const update = await check();
+      console.log("update", update);
+
+      if (update) {
+        console.log(
+          `found update ${update.version} from ${update.date} with notes ${update.body}`
+        );
+        setUpdateMessage(
+          `found update ${update.version} from ${update.date} with notes ${update.body}`
+        );
+        let downloaded = 0;
+        let contentLength = 0;
+        // alternatively we could also call update.download() and update.install() separately
+        await update.downloadAndInstall((event) => {
+          switch (event.event) {
+            case "Started":
+              contentLength = event.data.contentLength as number;
+              setUpdateMessage(
+                `started downloading ${event.data.contentLength} bytes`
+              );
+              break;
+            case "Progress":
+              downloaded += event.data.chunkLength;
+              setUpdateMessage(
+                `downloaded ${downloaded} from ${contentLength}`
+              );
+              setUpdateProgress((downloaded / contentLength) * 100);
+              break;
+            case "Finished":
+              setUpdateProgress(100);
+              setUpdateMessage("download finished");
+              break;
+          }
+        });
+
+        console.log("update installed");
+        setIsUpdated(true);
+        await relaunch();
+      } else {
+        setIsUpdating(false);
+      }
+    };
+
+    setTimeout(() => {
+      checkUpdate();
+    }, 1000);
+  }, []);
+ 
+
+  if (isUpdating || isUpdated) {
+    return (
+      <MainWindowLoading>
+        <div>
+          <h1>{updateMessage}</h1>
+          {updateProgress > 0 && <p>Progress {updateProgress}</p>}
+
+          
+        </div>
+      </MainWindowLoading>
+    );
+  }
   return (
     <div
       className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
